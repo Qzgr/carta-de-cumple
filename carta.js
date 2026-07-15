38566e8962c90b846f7727ed77a4e6e93b74c94a
Quereb <querebgonzalez@gmail.com>
2026-07-14 22:29:46 -0600

    cambios realizados en el index.html y carta.js para cambiar la temática de San Valentín a cumpleaños. Se ha modificado la clase del contenedor principal de "valentines-day" a "birthday-surprise" y se han actualizado los selectores en el archivo carta.js para reflejar este cambio.

--- carta.js
+++ carta.js
@@ -1,12 +1,12 @@
 $(document).ready(function () {
-  $('.valentines-day').click(function () {
+  $('.birthday-surprise').click(function () {
     $('.envelope').css({
       animation: 'fall 3s linear 1',
       '-webkit-animation': 'fall 3s linear 1'
     });
 
     $('.envelope').fadeOut(800, function () {
-      $('.valentines-day .heart, .valentines-day .text, .valentines-day .front').hide();
+      $('.birthday-surprise .heart, .birthday-surprise .text, .birthday-surprise .front').hide();
 
       $('#card').css({ visibility: 'visible', opacity: 0, transform: 'scale(0.1)' });
       $('#card').animate({ opacity: 1 }, {
